30598a9da7bc12ebc08a7ac932c18db45a580c8a
Ruben Navarro Piris <ruben.navarro.piris@gmail.com>
2015-04-08 15:25:28 +0200

    reactivated Ctrl+D to close terminal

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -3,7 +3,7 @@ setopt hist_ignore_all_dups
 # do not share global history
 setopt no_share_history
 # forces the user to type exit or logout, instead of just pressing ^D
-setopt ignoreeof
+#setopt ignoreeof
 # prevents accidentally overwriting an existing file while clobbering
 setopt noclobber
 
